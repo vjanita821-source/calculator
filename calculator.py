1a48cdefdb8e57bc9fdc57fd66267a75b38cf858
janita <vjanita821@gmail.com>
2026-09-17 03:44:28 -0700

    Added two operator

--- calculator.py
+++ calculator.py
@@ -3,6 +3,10 @@ b = int(input("Enter second number: "))
 
 addition = a + b
 subtraction = a - b
+multiplication= a * b
+division= a / b
 
 print("Addition =", addition)
 print("Subtraction =", subtraction)
+print("Multiplication =", multiplication)
+print("Division =", division)
